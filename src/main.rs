0a5d3be85e453a78eabd5ad1b47bbd661379c9fb
maxlestage <maxlestage@icloud.com>
2022-06-23 20:09:47 +0200

    update with match

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,24 @@ use std::io;
 fn main() {
     println!("Hello, world!");
 
-    let mut operator = String::new();
-    let mut number1 = String::new();
-    let mut number2 = String::new();
+    let set_operator: char = get_operator_choice();
 
-    let set_operator: char = get_operator_choice(&mut operator);
-
-    let nb1 = get_number_1(&mut number1);
-
-    let nb2 = get_number_2(&mut number2);
+    let nb1: f32 = get_number();
+    let nb2: f32 = get_number();
 
     let result: f32 = set_calc(set_operator, nb1, nb2);
     println!("The result is: {}", result);
 }
 
-fn get_operator_choice(operator: &mut String) -> char {
+fn get_operator_choice() -> char {
+    let mut operator = String::new();
     println!("Please choose an operator for your calculation.");
     println!(
         " \nYou have the choice: \n\nSoustraction: -\nAddition: +\nDivision: /\nMultiplication: *\n"
     );
 
     io::stdin()
-        .read_line(operator)
+        .read_line(&mut operator)
         .expect("Failed to read line");
 
     let operator: char = operator.trim().chars().next().unwrap();
@@ -34,35 +30,27 @@ fn get_operator_choice(operator: &mut String) -> char {
     operator
 }
 
-fn get_number_1(number1: &mut String) -> f32 {
+fn get_number() -> f32 {
+    let mut number = String::new();
     println!("Please type a number: ");
 
-    io::stdin().read_line(number1).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
 
-    let number1: f32 = number1.trim().parse().expect("Please type a number!");
-    number1
+    let number: f32 = number.trim().parse().expect("Please type a number!");
+    number
 }
 
-fn get_number_2(number2: &mut String) -> f32 {
-    println!("Please type a number: ");
-
-    io::stdin().read_line(number2).expect("Failed to read line");
-
-    let number2: f32 = number2.trim().parse().expect("Please type a number!");
-    number2
-}
-
-fn set_calc(set_operator: char, number1: f32, number2: f32) -> f32 {
+fn set_calc(set_operator: char, nb1: f32, nb2: f32) -> f32 {
     let mut result: f32 = 0.0;
 
-    if set_operator == '+' {
-        result = number1 + number2
-    } else if set_operator == '-' {
-        result = number1 - number2
-    } else if set_operator == '*' {
-        result = number1 * number2
-    } else if set_operator == '/' {
-        result = number1 / number2
+    match set_operator {
+        '+' => result = nb1 + nb2,
+        '-' => result = nb1 - nb2,
+        '*' => result = nb1 * nb2,
+        '/' => result = nb1 / nb2,
+        _ => println!("something else"),
     }
 
     result
